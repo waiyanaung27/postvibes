cb52705f37585331daf965d96d23c63392eabc6a
yethuaungptu <yethuaung481995@gmail.com>
2024-08-04 07:36:18 +0630

    [add] route and page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,18 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+router.get("/createPost", function (req, res) {
+  res.render("createpost");
+});
+
+router.get("/profile", function (req, res) {
+  res.render("profile");
+});
+
 router.get("/blank", function (req, res) {
   res.render("blank");
 });
