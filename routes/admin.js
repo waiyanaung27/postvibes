c4afdc8f806ab8f23ee833633fc97048e68ac15e
Colerone <min776893@gmail.com>
2024-08-24 23:21:06 +0630

    banAccount

--- routes/admin.js
+++ routes/admin.js
@@ -17,4 +17,24 @@ router.get("/uaccountlist", function (req,res){
     res.render("admin/uaccountlist");
 });
 
+router.get("/accountdetail", function (req,res){
+    res.render("admin/accountdetail");
+});
+
+router.get("/banpostlist", function(req,res){
+    res.render("admin/banpostlist");
+});
+
+router.get("/banpostdetail", function(req,res){
+    res.render("admin/banpostdetail");
+});
+
+router.get("/accountDetails", function(req, res){
+    res.render("admin/accountDetails");
+  });
+
+router.get("/banAccount", function(req, res){
+    res.render("admin/banAccount");
+  });
+
 module.exports = router;
