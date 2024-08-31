204ee27456739ebb89512f26c3995d197044eee9
yethuaungptu <yethuaung481995@gmail.com>
2024-08-31 13:09:27 +0630

    update admin panel

--- routes/admin.js
+++ routes/admin.js
@@ -1,40 +1,39 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/alog", function (req, res) {
-    res.render("admin/alog");
+router.get("/", function (req, res) {
+  res.render("admin/index");
+});
+router.get("/login", function (req, res) {
+  res.render("admin/login");
 });
 
-router.get("/create", function (req, res) {
-    res.render("admin/create");
+router.get("/register", function (req, res) {
+  res.render("admin/register");
 });
 
-router.get("/blank", function (req,res){
-    res.render("admin/blank");
+router.get("/blank", function (req, res) {
+  res.render("admin/blank");
 });
 
-router.get("/uaccountlist", function (req,res){
-    res.render("admin/uaccountlist");
+router.get("/uaccountlist", function (req, res) {
+  res.render("admin/uaccountlist");
 });
 
-router.get("/accountdetail", function (req,res){
-    res.render("admin/accountdetail");
+router.get("/accountdetail", function (req, res) {
+  res.render("admin/accountdetail");
 });
 
-router.get("/banpostlist", function(req,res){
-    res.render("admin/banpostlist");
+router.get("/banpostlist", function (req, res) {
+  res.render("admin/banpostlist");
 });
 
-router.get("/banpostdetail", function(req,res){
-    res.render("admin/banpostdetail");
+router.get("/banpostdetail", function (req, res) {
+  res.render("admin/banpostdetail");
 });
 
-router.get("/accountDetails", function(req, res){
-    res.render("admin/accountDetails");
-  });
-
-router.get("/banAccount", function(req, res){
-    res.render("admin/banAccount");
-  });
+router.get("/banAccount", function (req, res) {
+  res.render("admin/banAccount");
+});
 
 module.exports = router;
